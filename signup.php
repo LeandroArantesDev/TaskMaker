d8786a101d40234bc03eabf5182aff9f00592024
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-04-07 16:03:10 -0300

    Atualização css

--- signup.php
+++ signup.php
@@ -8,6 +8,8 @@ $_SESSION['_csrf'] = (isset($_SESSION['_csrf'])) ? $_SESSION['_csrf'] : hash('sh
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/variaveis.css">
+    <link rel="stylesheet" href="assets/css/signup.css">
     <title>TaskMaker | Registre-se</title>
 </head>
 
