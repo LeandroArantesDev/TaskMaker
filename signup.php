bbff397cad59c463e31428cea95c2e36386170e7
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-04-07 22:02:08 -0300

    Atualização CSS

--- signup.php
+++ signup.php
@@ -15,33 +15,31 @@ $_SESSION['_csrf'] = (isset($_SESSION['_csrf'])) ? $_SESSION['_csrf'] : hash('sh
 
 <body>
     <div class="form">
+        <p>Registre-se</p>
         <form action="auth/register.php" method="post">
             <input type="hidden" name="_csrf" value="<?php echo htmlentities($_SESSION['_csrf']) ?>">
 
             <div class="form-group">
-                <label for="nome">Nome</label>
                 <input type="text" name="nome" id="nome" required pattern="[A-Za-zÀ-ÿ\s]{2,}"
-                    title="Digite um nome válido (apenas letras e espaços)">
+                    title="Digite um nome válido (apenas letras e espaços)" placeholder="Digite seu nome">
             </div>
             <div class="form-group">
-                <label for="sobrenome">Sobrenome</label>
                 <input type="text" name="sobrenome" id="sobrenome" required pattern="[A-Za-zÀ-ÿ\s]{2,}"
-                    title="Digite um sobrenome válido (apenas letras e espaços)">
+                    title="Digite um sobrenome válido (apenas letras e espaços)" placeholder="Digite seu sobrenome">
             </div>
             <div class="form-group">
-                <label for="email">E-mail</label>
-                <input type="email" name="email" id="email" required>
+                <input type="email" name="email" id="email" required placeholder="Digite seu email">
             </div>
             <div class="form-group">
-                <label for="senha">Senha</label>
                 <input type="password" name="senha" id="senha" required
                     pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}"
-                    title="A senha deve ter no mínimo 8 caracteres, com letras maiúsculas, minúsculas, números e símbolos.">
+                    title="A senha deve ter no mínimo 8 caracteres, com letras maiúsculas, minúsculas, números e símbolos."
+                    placeholder="Digite sua Senha">
             </div>
 
             <div class="form-group">
-                <label for="confirmarsenha">Confirme a senha</label>
-                <input type="password" name="confirmarsenha" id="confirmarsenha" required>
+                <input type="password" name="confirmarsenha" id="confirmarsenha" required
+                    placeholder="Confirme sua Senha">
             </div>
 
             <button type="submit">Registrar-se</button>
