cf7a6a6c49329ff281cde5688dc0153e32200406
LeandroArantesDev <leandro.arantes@edu.uniube.br>
2025-04-09 01:03:10 -0300

    Finalizado CSS do formulário de sign-up

--- signup.php
+++ signup.php
@@ -8,28 +8,31 @@ $_SESSION['_csrf'] = (isset($_SESSION['_csrf'])) ? $_SESSION['_csrf'] : hash('sh
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/variaveis.css">
     <link rel="stylesheet" href="assets/css/signup.css">
     <title>TaskMaker | Registre-se</title>
 </head>
 
 <body>
     <form action="auth/register.php" method="post">
-        <p>Registre-se</p>
+        <p>Formulário de cadastro</p>
         <input type="hidden" name="_csrf" value="<?php echo htmlentities($_SESSION['_csrf']) ?>">
 
         <div class="form-group">
+            <label for="nome">Nome</label>
             <input type="text" name="nome" id="nome" required pattern="[A-Za-zÀ-ÿ\s]{2,}"
                 title="Digite um nome válido (apenas letras e espaços)" placeholder="Digite seu nome">
         </div>
         <div class="form-group">
+            <label for="sobrenome">Sobrenome</label>
             <input type="text" name="sobrenome" id="sobrenome" required pattern="[A-Za-zÀ-ÿ\s]{2,}"
                 title="Digite um sobrenome válido (apenas letras e espaços)" placeholder="Digite seu sobrenome">
         </div>
         <div class="form-group">
+            <label for="email">E-mail</label>
             <input type="email" name="email" id="email" required placeholder="Digite seu email">
         </div>
         <div class="form-group">
+            <label for="senha">Senha</label>
             <input type="password" name="senha" id="senha" required
                 pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}"
                 title="A senha deve ter no mínimo 8 caracteres, com letras maiúsculas, minúsculas, números e símbolos."
@@ -37,7 +40,9 @@ $_SESSION['_csrf'] = (isset($_SESSION['_csrf'])) ? $_SESSION['_csrf'] : hash('sh
         </div>
 
         <div class="form-group">
-            <input type="password" name="confirmarsenha" id="confirmarsenha" required placeholder="Confirme sua Senha">
+            <label for="confirmarsenha">Confirme sua senha</label>
+            <input type="password" name="confirmarsenha" id="confirmarsenha" required
+                placeholder="Digite a senha novamente">
         </div>
         <button type="submit">Registrar-se</button>
     </form>
